3ca9e171beb46d2d14346a72f1657a719b8b4bac
Gerard Braad <me@gbraad.nl>
2016-08-09 17:34:34 +0800

    Download over http :-s

--- alpine/run_container.sh
+++ alpine/run_container.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://storage.googleapis.com/kubernetes-release/release/v1.3.4/bin/linux/amd64/kubectl -O /usr/sbin/kubectl
-chmod +x kubectl
+wget http://storage.googleapis.com/kubernetes-release/release/v1.3.4/bin/linux/amd64/kubectl -O /usr/sbin/kubectl
+chmod +x /usr/sbin/kubectl
 
